9a2c4e1e26df8bf1a60d1cd438d9d0362447f354
tuyendt <tuyendt@gmail.com>
2024-10-22 19:12:07 +0700

    init base

---
+++ chrono-unify-web/src/main/java/thedtt/project/chronounifyweb/ChronoUnifyWebApplication.java
@@ -0,0 +1,13 @@
+package thedtt.project.chronounifyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronoUnifyWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronoUnifyWebApplication.class, args);
+	}
+
+}
---
+++ chrono-unify-web/src/test/java/thedtt/project/chronounifyweb/ChronoUnifyWebApplicationTests.java
@@ -0,0 +1,13 @@
+package thedtt.project.chronounifyweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronoUnifyWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
